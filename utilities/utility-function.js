bb6499f4032368a68d90aa2f8258c9ba060c60e8
Keyur Halpati <krhalpati@gmail.com>
2025-01-19 16:54:03 +0530

    add multer common function

--- utilities/utility-function.js
+++ utilities/utility-function.js
@@ -16,5 +16,27 @@ module.exports = {
         }
         logger.info(`${self.jsonConst.LOG_EXIT} ${self.jsonConst.LOG_FUNCTION} userJoiValidator() ${self.jsonConst.LOG_SUCCESS}`);
         next();
-    }
+    },
+
+    upload: multer({
+        storage: multer.diskStorage({
+            destination: function (req, file, cb) {
+                cb(null, path.join(__dirname, '../app/assets/')); // Save files to this directory
+            },
+            filename: function (req, file, cb) {
+                cb(null, `${Date.now()}-${file.originalname}`); // Custom filename
+            }
+        }),
+        limits: { fileSize: 2 * 1024 * 1024 }, // 2MB limit
+        fileFilter: function (req, file, cb) {
+            const fileTypes = /jpeg|jpg|png|gif/; // Allowed image types
+            const extName = fileTypes.test(path.extname(file.originalname).toLowerCase());
+            const mimeType = fileTypes.test(file.mimetype);
+            if (extName && mimeType) {
+                cb(null, true);  // Accept file
+            } else {
+                cb(new Error('Only image files (jpeg, jpg, png, gif) are allowed!'));  // Reject file
+            }
+        }
+    }).array('files'),
 };
