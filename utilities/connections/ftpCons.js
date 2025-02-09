e7588ddee8be1f8fe12c9db6d13dd7b1081ba326
Keyur Halpati <krhalpati@gmail.com>
2025-02-09 19:21:48 +0530

    ftp

---
+++ utilities/connections/ftpCons.js
@@ -0,0 +1,16 @@
+const ftp = require('basic-ftp');
+const func = require('../utility-function');
+
+async function ftpClient() {
+    const client = new ftp.Client();
+    client.ftp.verbose = true;
+    try {
+        await client.access({ host: process.env.FTP_HOST, port: process.env.FTP_PORT, user: process.env.FTP_USER, password: process.env.FTP_PASS, secure: false, });
+        return client
+    } catch (error) {
+        logger.error(`${func.jsonConst.LOG_EXIT} ${func.jsonConst.LOG_FUNCTION}  Error connecting to FTP server:`, error);
+        throw error;
+    }
+}
+
+module.exports = ftpClient;
--- utilities/utility-function.js
+++ utilities/utility-function.js
@@ -1,8 +1,11 @@
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs')
+const ftp = require('../utilities/connections/ftpCons');
 
 module.exports = {
     // constants links
-    jsonConst: require('./constents/jsonConst'),
-
+    jsonConst: require('./constants/jsonConst'),
 
 
     // functions
@@ -38,5 +41,71 @@ module.exports = {
                 cb(new Error('Only image files (jpeg, jpg, png, gif) are allowed!'));  // Reject file
             }
         }
-    }).array('files'),
+    }).any(),
+
+    uploadFile: async function (reqFiles) {
+        const self = this;
+        const client = await ftp()
+        try {
+            const mainDir = 'sdcard';
+            const subDir = 'ftpServer';
+
+            // console.log("Listing files in the root directory:");
+            // const customDir = (await client.list("/")).find(file => file.name === mainDir && file.isDirectory);
+            // if (!customDir) {
+            //     logger.info(`given directory not found! so Created that directory`);
+            //     await client.ensureDir(`/sdcard`);
+            // }
+
+            await client.ensureDir(`/${mainDir}/${subDir}`);
+
+            if (fs.existsSync(reqFiles.path)) {
+                await client.uploadFrom(reqFiles.path, `/${mainDir}/${subDir}/${reqFiles.filename}`);
+                logger.info(`${self.jsonConst.LOG_EXIT} ${self.jsonConst.LOG_FUNCTION} uploadFile() ${self.jsonConst.LOG_SUCCESS}`);
+                return `/${mainDir}/${subDir}/${reqFiles.filename}`
+            } else {
+                logger.error(`${self.jsonConst.LOG_EXIT} ${self.jsonConst.LOG_FUNCTION} uploadFile() ${self.jsonConst.LOG_ERROR} Local file does not exist.`);
+            }
+        } catch (err) {
+            logger.error(`${self.jsonConst.LOG_EXIT} ${self.jsonConst.LOG_FUNCTION} uploadFile()`, err);
+        }
+        client.close();
+    },
+
+    getImageBufferFromFTP: async function (remoteImagePath) {
+        const self = this;
+        const client = await ftp()
+        const imagePath = path.join(path.join(__dirname, '../app/assets/'), path.basename(remoteImagePath));
+
+        try {
+            await client.downloadTo(imagePath, remoteImagePath);
+            self.deleteImageAfterTimeout(imagePath, 1 * 60 * 1000);
+
+            return imagePath;
+        } catch (error) {
+            logger.error(`${self.jsonConst.LOG_EXIT} ${self.jsonConst.LOG_FUNCTION} Error retrieving image from FTP server:`, error);
+            throw error;
+        } finally {
+            client.close();
+        }
+    },
+
+    deleteImageAfterTimeout: function (filePath, delay) {
+        const self = this
+        setTimeout(() => {
+            if (fs.existsSync(filePath)) {
+                fs.unlink(filePath, (err) => {
+                    if (err) {
+                        logger.error(`${self.jsonConst.LOG_EXIT} ${self.jsonConst.LOG_FUNCTION} Error deleting image:`, err);
+                    } else {
+                        // console.log(`Image deleted successfully: ${filePath}`);
+                    }
+                });
+            }
+        }, delay);
+    },
+
+    removeFile: function (reqFiles) {
+        fs.unlinkSync(reqFiles);
+    },
 };
